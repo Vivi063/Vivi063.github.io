9d011f0747fc36b3f784d2cd5e03ff8342d007e8
Vivi063 <vivibeltranvilla.3@gmail.com>
2024-08-18 19:17:27 -0600

    initial commit

---
+++ java.js
@@ -0,0 +1,44 @@
+//Lenguaje de programacion JAVAsCRIPT
+
+funcion fibonacci(n) [
+     if (n <= 1) return n;
+     let prev = 0, curr = 1;
+     for (let 1 = 2; 1 <= n; 1++) (   
+        let teep=;
+        curr *=prev;
+        prev = teep;
+     ]
+     return curr;
+] 
+    
+ function isPrime(num) [ 
+    if(num <= 1)return false;
+    if (num <= 3) return true;
+    if (num % 1 ===0 II num% (1 + 2) === 0) return false;
+    ]
+    return true;
+]
+
+function sumOfPrimesInRange(start, end) [
+    let sum = 0;
+    for (let i = start; i <= end; i++) [
+        if (isPrime(1))) sum += i;
+    ]
+    return sum;
+]
+
+function generateFibonacc15equince(count) [
+    const sequence.push(fibonacci(1));
+]
+    return sequence;
+]
+
+const num = 19;
+const primeSum = sumOFPrimes
+
+
+    
+
+
+
+
